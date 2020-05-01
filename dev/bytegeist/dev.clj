52501bd4f55383d219315bbc89a3e74e4971bcde
Yonatan Elhanan <yonatan.elhanan@gmail.com>
2020-05-01 13:47:30 +0300

    Setup dev

---
+++ dev/bytegeist/dev.clj
@@ -0,0 +1,1 @@
+(ns bytegeist.dev)
--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject bytegeist "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "Schemas for binary data"
+  :url "https://github.com/yonatane/bytegeist"
+  :license {:name "MIT License"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.netty/netty-buffer "4.1.49.Final"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "1.0.0"]
+                                  [criterium "0.4.5"]
+                                  [com.clojure-goes-fast/clj-async-profiler "0.4.1"]]}}
+  :repl-options {:init-ns bytegeist.dev}
+  :global-vars {*warn-on-reflection* true}
+  :pedantic? :abort)

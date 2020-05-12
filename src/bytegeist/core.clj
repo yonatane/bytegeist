90b414951671c6f4b102e28af6b94e0f01d4931d
Yonatan Elhanan <yonatan.elhanan@gmail.com>
2020-05-12 12:22:19 +0300

    Add Spec protocol with basic implementations and spec manipulation

--- src/bytegeist/core.clj
+++ src/bytegeist/core.clj
@@ -1,6 +1,107 @@
-(ns bytegeist.core)
+(ns bytegeist.core
+  (:refer-clojure :exclude [get read set])
+  (:import (io.netty.buffer ByteBuf)
+           (java.nio ByteBuffer)
+           (java.nio.charset StandardCharsets)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- shape
+  [s]
+  (if (vector? s)
+    (first s)
+    (throw (IllegalArgumentException. "Only supporting vectors currently"))))
+
+(defn- map-shape?
+  [s]
+  (= :map (shape s)))
+
+(defn- vector-shape?
+  [s]
+  (= :vector (shape s)))
+
+(defn- append
+  [s fields]
+  (conj s fields))
+
+(defn- override?
+  [field]
+  (= :override (first field)))
+
+(defn- remove-override-tag
+  [field]
+  (vec (rest field)))
+
+(defn- normalize-field
+  [field]
+  (cond-> field (override? field) remove-override-tag))
+
+(defn- vector-add-fields
+  [s fields]
+  (throw (Exception. "vector-add-fields not implemented yet")))
+
+(defn- map-fields
+  [s]
+  (rest s))
+
+(defn- field-name
+  [field]
+  (first field))
+
+(defn- contains-field?
+  [coll field]
+  (some #{(field-name field)} (map field-name coll)))
+
+(defn map-add-fields
+  [s fields]
+  (let [; Ordered normalized fields
+        all-ordered
+        (mapv normalize-field (concat (map-fields s) fields))
+
+        ; Map field-name to its overridden type
+        final-types
+        (into {} all-ordered)
+
+        ; Reconstruct the map fields, old fields retain their position but their type might be overridden.
+        final-fields
+        (reduce
+          (fn [r field]
+            (let [fname (field-name field)]
+              (if (contains-field? r field)
+                r
+                (conj r [fname (clojure.core/get final-types fname)]))))
+          []
+          all-ordered)]
+
+    (into [:map] final-fields)))
+
+(defn add-fields [s fields]
+  (cond
+    (map-shape? s)
+    (map-add-fields s fields)
+
+    (vector-shape? s)
+    (vector-add-fields s fields)
+
+    :else
+    (throw (IllegalArgumentException. "Unknown shape passed to add-fields"))))
+
+(defprotocol Spec
+  (read [_ b] "Relative read and increment the index")
+  (write [_ b v] "Relative write and increment the index"))
+
+;TODO have a protocol for ByteBuf to avoid type hints
+
+(def int16
+  (reify
+    Spec
+    (read [_ b]
+      (.readShort ^ByteBuf b))
+    (write [_ b v]
+      (.writeShort ^ByteBuf b (int v)))))
+
+(def int32
+  (reify
+    Spec
+    (read [_ b]
+      (.readInt ^ByteBuf b))
+    (write [_ b v]
+      (.writeInt ^ByteBuf b (int v)))))
--- test/bytegeist/core_test.clj
+++ test/bytegeist/core_test.clj
@@ -1,7 +1,80 @@
 (ns bytegeist.core-test
-  (:require [clojure.test :refer :all]
-            [bytegeist.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [bytegeist.core :as g])
+  (:import (io.netty.buffer Unpooled ByteBuf)
+           (java.nio.charset StandardCharsets)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def nullable-string
+  (reify
+    g/Spec
+    (read [_ b]
+      (let [len (.readShort ^ByteBuf b)]
+        (if (< len 0)
+          nil
+          (.readCharSequence ^ByteBuf b len StandardCharsets/UTF_8))))
+    (write [_ b v]
+      (if (nil? v)
+        (.writeShort ^ByteBuf b (int -1))
+        (let [byts (.getBytes ^String v StandardCharsets/UTF_8)]
+          (.writeShort ^ByteBuf b (int (alength byts)))
+          (.writeBytes ^ByteBuf b byts))))))
+
+(def message
+  [:map
+   [:size :int32]])
+
+(def request-header-v0
+  [:map
+   [:api-key :int16]
+   [:api-version :int16]
+   [:correlation-id :int32]])
+
+(def request-header-v1
+  (-> request-header-v0
+      (g/add-fields [[:client-id nullable-string]])))
+
+(deftest spec
+  (testing "Spec new version from previous"
+    (let [v0 [:map [:a :int32]]
+          v1 (g/add-fields v0 [[:b :int16]])
+          literal-v1 [:map
+                      [:a :int32]
+                      [:b :int16]]]
+      (is (= literal-v1 v1)))))
+
+(deftest write-and-read
+  (testing "primitives"
+    (testing "int16"
+      (let [^g/Spec s g/int16
+            b (Unpooled/buffer 2 2)
+            v 1]
+        (g/write s b v)
+        (is (= v (g/read s b)))))
+    (testing "int32"
+      (let [^g/Spec s g/int32
+            b (Unpooled/buffer 4 4)
+            v 1]
+        (g/write s b v)
+        (is (= v (g/read s b)))))
+    (testing "nullable-string nil"
+      (let [^g/Spec s nullable-string
+            b (Unpooled/buffer 2 2)
+            v nil]
+        (g/write s b v)
+        (is (= v (g/read s b)))))
+    (testing "nullable-string empty"
+      (let [^g/Spec s nullable-string
+            b (Unpooled/buffer 2 2)
+            v ""]
+        (g/write s b v)
+        (is (= v (g/read s b)))))
+    (testing "nullable-string non-empty"
+      (let [^g/Spec s nullable-string
+            b (Unpooled/buffer)
+            v "a b c"]
+        (g/write s b v)
+        (is (= v (g/read s b))))))
+  (testing "map"
+    (testing "with int32"
+      (let [s [:map [:a :int32]]]
+        (is (= true false) "TODO")))))
